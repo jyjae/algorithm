8bc38920b91ff1fa3536b507a45cadd8f594a4d5
yeonjaery <yeonjaery@google.com>
2022-10-14 19:13:36 +0900

    이코테 8장 문제

---
+++ 개미전사.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 개미전사 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] d = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        d[0] = arr[0];
+        d[1] = Math.max(arr[0], arr[1]);
+
+        for(int i=2; i<N; i++) {
+            d[i] = Math.max(d[i-1], d[i-2]+arr[i]);
+        }
+
+        System.out.println(d[N-1]);
+    }
+}
